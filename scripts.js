15b056ce7ef070ff6325815b7744100d449b1c57
ruserxd <zxcmomo0219@gmail.com>
2024-05-30 15:06:30 +0800

    give the music player and google form

--- scripts.js
+++ scripts.js
@@ -1,3 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function() {
     document.body.classList.add('fade-in');
+
+    var audio = document.getElementById('audio');
+    var playPauseBtn = document.getElementById('play-pause-btn');
+    var playPauseIcon = playPauseBtn.querySelector('i');
+
+    playPauseBtn.addEventListener('click', function() {
+        if (audio.paused || audio.ended) {
+            audio.play();
+            playPauseIcon.className = 'fas fa-pause'; // 更改为暂停图标
+        } else {
+            audio.pause();
+            playPauseIcon.className = 'fas fa-play'; // 更改为播放图标
+        }
+    });
 });
